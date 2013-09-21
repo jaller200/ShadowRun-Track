18e930d08db11b233d09019d3a341445f0a28944
The Doctor <jonathan@ehart.gateway.2wire.net>
2013-09-21 16:06:24 -0500

    Initial Commit

---
+++ ShadowRun/ShadowRunAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ShadowRunAppDelegate.h
+//  ShadowRun
+//
+//  Created by The Doctor on 9/21/13.
+//  Copyright (c) 2013 ShadowPress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShadowRunAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShadowRun/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShadowRun
+//
+//  Created by The Doctor on 9/21/13.
+//  Copyright (c) 2013 ShadowPress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ShadowRunAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ShadowRunAppDelegate class]));
+    }
+}
